69d096940bdd51c1444797252f07d8d1966e5c98
Marianoooo45 <aurelien.gouyou@gmail.com>
2025-08-09 21:45:12 +0200

    Delete api directory

--- api/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class Job(BaseModel):
-    id: str
-    title: str
-    link: str
-    posted: datetime
-    source: str          # DB, SG, …
