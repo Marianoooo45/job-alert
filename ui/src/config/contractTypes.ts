5844414d9c161e4c051103ffe58547540ec253d2
Marianoooo45 <aurelien.gouyou@gmail.com>
2025-08-31 19:16:57 +0200

    Update contractTypes.ts

--- ui/src/config/contractTypes.ts
+++ ui/src/config/contractTypes.ts
@@ -5,8 +5,6 @@ export const CONTRACT_TYPE_LIST: ContractType[] = [
   { id: "cdd", name: "CDD" },
   { id: "stage", name: "Stage" },
   { id: "alternance", name: "Alternance" },
-  { id: "freelance", name: "Freelance" },
   { id: "vie", name: "VIE" },
   { id: "non-specifie", name: "Non spécifié" },
-  { id: "autre", name: "Autre" },
 ];
